79d3b30924f454de2ac8d8276a20caab89c6d8f7
Ashish Singh <ashishsng21@hotmail.com>
2020-12-24 17:26:55 -0600

    Gave customize views by modifying controller.

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -24,6 +24,9 @@ namespace EmployeeManagement.Controllers
         {
             Employee model = _employeeRepository.GetEmployeeById(1);
             return View(model);
+            return View(); // Since it has not parameter it look for Details.cstml in Home controller
+            return View("Test"); // This will look for Test.cshtml in View/Home folder.
+            return View("../../NewFolder/newview.cshtml") // This is relative path. It was in the home folder. first it came out from home folder, then view folder which means it come to root folder. From root folder it went to New folder and then looked for newview.cshtml file.
         }
     }
 }
